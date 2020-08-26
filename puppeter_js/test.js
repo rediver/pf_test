41c2017e5974b5b899a377c3bec144c044935f47
Wojtek <wojciech.smajda@gmail.com>
2020-08-26 14:10:03 +0200

    add puppeter sample

---
+++ puppeter_js/test.js
@@ -0,0 +1,27 @@
+const puppeteer = require('puppeteer');
+(async () => {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  
+  const navigationPromise = page.waitForNavigation()
+  
+  await page.goto('https://www.selenium.dev/selenium-ide/docs/en/plugins/plugins-getting-started')
+  
+  await page.setViewport({ width: 1920, height: 949 })
+  
+  await page.waitForSelector('.navGroups > .navGroup:nth-child(2) > ul > .navListItem:nth-child(3) > .navItem')
+  await page.click('.navGroups > .navGroup:nth-child(2) > ul > .navListItem:nth-child(3) > .navItem')
+  
+  await navigationPromise
+  
+  await page.waitForSelector('div > span > ul > li:nth-child(3) > a')
+  await page.click('div > span > ul > li:nth-child(3) > a')
+  
+  await navigationPromise
+  
+  await navigationPromise
+  
+  await navigationPromise
+  
+  await browser.close()
+})()
