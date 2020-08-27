1b906efcea0060f8582d92f8a1de9fee82a45a53
Wojtek <wojciech.smajda@gmail.com>
2020-08-27 14:13:49 +0200

    fix in exec

--- pdf/index.js
+++ pdf/index.js
@@ -2,13 +2,14 @@ module.exports = async function (browser, inputParameters) {
 
 
 const main = async () => {
+
   const page = await browser.newPage()
   await page.goto('https://news.ycombinator.com', { waitUntil: 'networkidle2' })
-  await page.pdf({ path: process.env.ARTIFACTS_PATH  + '/' + 'dupa.pdf', format: 'A4' })
+  await page.pdf({ path: process.env.ARTIFACTS_PATH  + '/' + 'abc.pdf' })
 
   await browser.close()
 }
 
-main()
+await main()
 
 };
