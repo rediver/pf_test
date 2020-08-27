4e5690376b0cbc744c76051cdccbdd5708c92d5e
Wojtek <wojciech.smajda@gmail.com>
2020-08-27 17:07:54 +0200

    path fixes

--- pf-test/index.js
+++ pf-test/index.js
@@ -5,12 +5,14 @@ module.exports = async function (browser, inputParameters) {
 
   await page.goto('https://staging.powerfarm.ai/signin');
 
+  await page.setViewport({ width: 1920, height: 949 })
+
+
   await page.type('div #PolarisTextField1', 'aws-staging@makeitright.ai');
-  await page.screenshot({ path: 'login.png' })
+  await page.screenshot({path: process.env.SCREENSHOTS_PATH + '/' + 'login.png'})
 
   await page.type('div #PolarisTextField2', 'LEES.faug2nusk7bag');
-
-  await page.screenshot({ path: 'password.png' })	   
+  await page.screenshot({path: process.env.SCREENSHOTS_PATH + '/' + 'password.png'})	   
 
   await page.keyboard.press('Enter');	
 
@@ -22,17 +24,17 @@ module.exports = async function (browser, inputParameters) {
   await page.waitForSelector('.Polaris-Navigation__Section > .Polaris-Navigation__ListItem:nth-child(4) > .Polaris-Navigation__ItemWrapper > .Polaris-Navigation__Item > .Polaris-Navigation__Text')
   await page.click('.Polaris-Navigation__Section > .Polaris-Navigation__ListItem:nth-child(4) > .Polaris-Navigation__ItemWrapper > .Polaris-Navigation__Item > .Polaris-Navigation__Text')
   
-  await page.screenshot({ path: 'workflows.png' })	   
+  await page.screenshot({path: process.env.SCREENSHOTS_PATH + '/' + 'workflows.png'})  
 
   await page.waitForSelector('.Polaris-Navigation__Section:nth-child(1) > .Polaris-Navigation__ListItem:nth-child(3) > .Polaris-Navigation__ItemWrapper > .Polaris-Navigation__Item > .Polaris-Navigation__Text')
   await page.click('.Polaris-Navigation__Section:nth-child(1) > .Polaris-Navigation__ListItem:nth-child(3) > .Polaris-Navigation__ItemWrapper > .Polaris-Navigation__Item > .Polaris-Navigation__Text')  
 
-  await page.screenshot({ path: 'tasks.png' })	   
+  await page.screenshot({path: process.env.SCREENSHOTS_PATH + '/' + 'tasks.png'})   
 
   await page.waitForSelector('.Polaris-Navigation__Section:nth-child(1) > .Polaris-Navigation__ListItem:nth-child(2) > .Polaris-Navigation__ItemWrapper > .Polaris-Navigation__Item > .Polaris-Navigation__Text')
   await page.click('.Polaris-Navigation__Section:nth-child(1) > .Polaris-Navigation__ListItem:nth-child(2) > .Polaris-Navigation__ItemWrapper > .Polaris-Navigation__Item > .Polaris-Navigation__Text')
 
-  await page.screenshot({ path: 'schedule.png' })	   
+  await page.screenshot({path: process.env.SCREENSHOTS_PATH + '/' + 'schedule.png'}) 
 
 
 })()
