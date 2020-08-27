00b8bfc9605e04eb749725a9cd43671f951d0606
Wojtek <wojciech.smajda@gmail.com>
2020-08-27 15:20:09 +0200

    url change

--- pupp_unsplash/index.js
+++ pupp_unsplash/index.js
@@ -2,7 +2,7 @@ module.exports = async function (browser, inputParameters) {
 
 const main = async () => {
   const page = await browser.newPage()
-  await page.goto('https://unsplash.com')
+  await page.goto('https://news.ycombinator.com')
   await page.screenshot({path: process.env.SCREENSHOTS_PATH + '/' + 'screenshot.png'})
   await browser.close()
 }
