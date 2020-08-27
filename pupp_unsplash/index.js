e22021dc4523c9e45818eeaba848025919029a9d
Wojtek <wojciech.smajda@gmail.com>
2020-08-27 12:11:45 +0200

    mod unsplash

--- pupp_unsplash/index.js
+++ pupp_unsplash/index.js
@@ -1,12 +1,12 @@
-const puppeteer = require('puppeteer')
+module.exports = async function (browser, inputParameters) {
 
 const main = async () => {
-  const browser = await puppeteer.launch()
   const page = await browser.newPage()
   await page.goto('https://unsplash.com')
-  await page.screenshot({ path: 'unsplash.png' })
-
+  await page.screenshot({path: process.env.SCREENSHOTS_PATH + '/' + 'screenshot.png'})
   await browser.close()
 }
 
-main()
+await main()
+
+};
